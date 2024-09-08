a81dbfbf992c3d1e30747a42664f3913abb3642e
lodu1233 <001cryptography@gmail.com>
2024-09-08 14:54:26 +0000

    feat: initiated the project

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("lodi","lodi")
+        ERC721("okok","klkl")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
